176935faeaadd84e471df910ee0ed4655a00085c
justworx <justworx@protonmail.com>
2019-03-02 09:07:17 -0600

    propstr - experimental

---
+++ propx/propstr.py
@@ -0,0 +1,20 @@
+#
+# Copyright 2019 justworx
+# This file is part of the trix project, distributed under 
+# the terms of the GNU Affero General Public License.
+#
+
+from .propseq import *
+
+
+class propstr(propseq):
+	pass
+
+
+	#
+	# I think there's a lot this class could do, but I don't want to
+	# make time to work on it until the rest of the classes are more
+	# stable.
+	#
+
+
