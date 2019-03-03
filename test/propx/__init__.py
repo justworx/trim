6b75216e2329974f8f1b71d54ca59d1fa8a19da0
justworx <justworx@protonmail.com>
2019-03-03 13:38:28 -0600

    add some propx tests

--- test/propx/__init__.py
+++ test/propx/__init__.py
@@ -1,27 +1,20 @@
 
 
 
-from .. import *
+from ..propx import *
 
 # test propx creation types
 assert(type(propx([1,2])).__name__  == "proplist")
-assert(type(propx("1,2")).__name__  == "propseq")
+assert(type(propx("1,2")).__name__  == "propstr")
 assert(type(propx(b"1,2")).__name__  == "propseq")
-assert(type(propx((1,2))).__name__  == "propset")
-assert(type(propx(tuple([1,2]))).__name__  == "propset")
+assert(type(propx((1,2))).__name__  == "propseq")
+assert(type(propx(tuple([1,2]))).__name__  == "propseq")
 assert(type(propx(iter([1,2]))).__name__  == "propiter")
 
 
 #test propiter
 ii = propx(iter([1,5,3,2,7]))
 ii.sorted()
-
-
-#
-# END TEST
-#
-print("* Module `trix.propx`  : OK\n")
-
 
 
 
