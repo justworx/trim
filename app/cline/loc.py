409833cc895eba5f190590fe5d2b98e0f20cc55a
justworx <justworx@protonmail.com>
2019-03-06 18:43:15 -0600

    start cline for locale data

--- app/cline/loc.py
+++ app/cline/loc.py
@@ -1,4 +1,4 @@
-#
++#
 # Copyright 2019 justworx
 # This file is part of the trix project, distributed under the terms 
 # of the GNU Affero General Public License.
@@ -20,5 +20,35 @@ class loc(cline):
 	def __init__(self):
 		cline.__init__(self)
 		locale.setlocale(locale.LC_ALL, self.args[0])
-		trix.display(locale.localeconv())
+		
+		rdict = {}
+		rdict['convert'] = locale.localeconv()
+		rdict['format'] = {
+			"datetime" : locale.nl_langinfo(locale.D_T_FMT),
+			"date" : locale.nl_langinfo(locale.D_FMT),
+			"time" : locale.nl_langinfo(locale.T_FMT),
+			"ampm" : locale.nl_langinfo(locale.T_FMT_AMPM),
+			"era" : locale.nl_langinfo(locale.ERA)
+		}
+		trix.display()
 
+	#
+	# Formats
+	#  - This section returns format strings
+	#
+	#
+	def datetime(self):
+		"""Localized datetime format string."""
+		return self.info(locale.D_T_FMT)
+	
+	def date(self):
+		"""Localized date format string."""
+		return self.info(locale.D_FMT)
+	
+	def time(self):
+		"""Localized time format string."""
+		return self.info(locale.T_FMT)
+	
+	def ampm(self):
+		"""Localized AM/PM strings."""
+		return self.info(locale.T_FMT_AMPM)
