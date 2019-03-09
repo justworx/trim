2242f89a7a5c5379c1fa4922aeb798021a4f2a1c
justworx <justworx@protonmail.com>
2019-03-09 13:45:29 -0600

    prep for big, experimental changes

--- x/loc.py
+++ x/loc.py
@@ -8,104 +8,26 @@ from .. import *
 import locale
 
 
-class Locale(object):
-	"""Stores formatting values and strings for the given locale."""
+class BaseLocale(object):
+	"""Base for Locale and AltLocale subclasses."""
 	
-	@classmethod
-	def query_locale_dict(cls, locale=None):
-		"""
-		Pass a locale string, eg., "en_US.UTF_8", etc... Returns a new
-		`trix.x.loc` object containing locale format methods and data.
-		"""
-		locmod = trix.module("locale")
-		locstr = locale or ".".join(locmod.getlocale())
-		py_ver = 'python3' if sys.version_info[0]==3 else "python"
-		cline = "%s -m %s loc %s" % (py_ver, trix.innerfpath(), locstr)
-		
-		proc = trix.popen(cline)
-		jsonb = proc.communicate()[0]
-		jsons = jsonb.decode("UTF_8")
-		
-		return trix.jparse(jsons)
-		#return trix.ncreate("x.loc.Locale", trix.jparse(jsons))
+
+class Locale(BaseLocale):
+	"""
+	Locale wraps the locale module's features. It's intended for use
+	when generating output expected by the user of *this* application.
+	There should be only one Locale object in any given application; it
+	should be created when the application starts and persist through
+	the process runtime.
 	
-	
-	def __init__(self, locinfo=None):
-		"""
-		Pass argument `locinfo` as a locale signature string (Eg.,
-		"en_US.UTF8") or as a dict containing the full set of key/value
-		pairs required to support this object's operation.
-		"""
-		
-		try:
-			#
-			# If locinfo has a getkeys method, it's a dict and should be
-			# stored directly in `self.__dict`.
-			#
-			locinfo.getkeys
-			self.__dict = locinfo
-		
-		except:
-			#
-			# Otherwise, locinfo must be a locale signature string in the
-			# required format. Eg., "en_US.UTF8". In this case, the string
-			# is passed to the `Locale.query_locale_dict` classmethod and
-			# a new locinfo dict will be generated (in a subprocess so as
-			# to avoid any potential quirks of python's `locale` module.) 
-			#
-			self.__dict = self.query_locale_dict(locinfo)
-		
-		#
-		# Store loc data as member variables
-		#
-		
-		# info
-		self.__locale = self.__dict['locale']
-		
-		# formats
-		self.__loconv = dict(self.__dict['loconv'])
-		self.__fmt = fmt = dict(self.__dict['format'])
-		
-		self.__f_dt = fmt["datetime"]
-		self.__f_d  = fmt["date"]
-		self.__f_t  = fmt["time"]
-		self.__fapm = fmt['ampm']
-		self.__fera = fmt['era']
-		
-		
-		#
-		# COMING SOON...
-		#  - values (days of week/month, and alts)
-		#
-	
-	
-	
-	@property
-	def locale(self):
-		return self.__locale
-	
-	@property
-	def format(self):
-		return self.__fmt
-	
-	@property
-	def conv(self):
-		return self.__loconv
-	
-	
-	# formats
-	def datetime(self, f=None):
-		return strftime(f or self.__f_dt)
-	
-	def date(self, f=None):
-		return strftime(self.__f_d)
-	
-	def time(self, f=None):
-		return strftime(self.__f_t)
-	
-	def ampm(self, f=None):
-		return strftime(self.__fapm)
-	
-	def era(self, f=None):
-		return strftime(self.__fera)
-	
+	To generate output strings in this app that contain formmatting for
+	other locales, use AltLocal.
+	"""
+
+
+class AltLocal(BaseLocale):
+	"""
+	AltLocale allows data formatting expected in other locales. Many 
+	AltLocale objects may be created, used, and discarded during the
+	lifespan of any given application process.
+	"""
