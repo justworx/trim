101f6e2ade0eb1f3f419c07f73b1e4f8a129b307
justworx <justworx@protonmail.com>
2019-03-10 11:19:30 -0500

    add 'query_local_dict'

--- x/loc.py
+++ x/loc.py
@@ -31,3 +31,26 @@ class AltLocal(BaseLocale):
 	AltLocale objects may be created, used, and discarded during the
 	lifespan of any given application process.
 	"""
+	
+	
+	#
+	# We'll put classmethods at the bottom for now. Looks like there
+	# are a few more coming.
+	#
+	@classmethod
+	def query_locale_dict(cls, locale=None):
+		"""
+		Pass a locale string, eg., "en_US.UTF_8", etc... Returns a new
+		`trix.x.loc` object containing locale format methods and data.
+		"""
+		locmod = trix.module("locale")
+		locstr = locale or ".".join(locmod.getlocale())
+		py_ver = 'python3' if sys.version_info[0]==3 else "python"
+		cline = "%s -m %s loc %s" % (py_ver, trix.innerfpath(), locstr)
+		
+		proc = trix.popen(cline)
+		jsonb = proc.communicate()[0]
+		jsons = jsonb.decode("UTF_8")
+		
+		return trix.jparse(jsons)
+		#return trix.ncreate("x.loc.Locale", trix.jparse(jsons))
