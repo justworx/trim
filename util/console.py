b813b5b6150137775ed5d44c25b3e494c5a5d522
justworx <justworx@protonmail.com>
2019-05-23 11:23:59 -0500

    overhaul console - on the right track now

--- util/console.py
+++ util/console.py
@@ -4,7 +4,7 @@
 # the terms of the GNU Affero General Public License.
 #
 
-import textwrap
+import textwrap, atexit
 from . import terminfo
 from .output import * # trix, enchelp, sys
 from .event import *
@@ -23,50 +23,69 @@ class Console(BaseOutput):
 	
 	Debug = 1 #0
 	
-	@classmethod
-	def oappend(cls, o):
-		try:
-			cls.__olist.append(o)
-		except:
-			cls.__olist = []
-			cls.__olist.append(o)
+	# need to make these defaults for `config`
+	ConsoleCmdKey  = '/'
+	ConsoleWrapKey = '!'
+	ConsolePrompt  = "trix.Console:"
+	ConsoleConfig  = "app/config/assets/console.%s" % DEF_LANG
 	
-	@classmethod
-	def oremove(cls, o):
-		try:
-			cls.__olist.remove(o)
-		except:
-			cls.__olist = []
-	
-	
-	
-	
+	TextK_Updates  = ["width", "expand_tabs", "tabsize", 
+		"replace_whitespace", "drop_whitespace", "initial_indent", 
+		"subsequent_indent", "fix_sentence_endings", "break_long_words",
+		"break_on_hyphens", "max_lines", "placeholder"]
+
 	
 	def __init__(self, config=None, **k):
 		
 		config = config or {}
 		config.update(k)
 		
-		#print ("k:", k)
-		wrap = trix.kpop(k, 'wrap').get('wrap') # object to wrap (or None)
-		#print ('wrap', wrap)
-		
-		
+		# init superclass
+		BaseOutput.__init__(self, config)
 		BaseOutput.__init__(self, config)
 		
-		# debugging
+		# set debugging
 		self.__debug = config.get('debug', self.Debug)
 		
 		# wrapper
+		wrap = trix.kpop(config, 'wrap').get('wrap')
 		self.__wrap = Wrap(wrap) if wrap else None
 		
+		# set wrapper name
+		wrapname = self.__wrap.name if self.__wrap else "None"
+		
 		# formatting for textwrap
-		jc = trix.jconfig(trix.npath('util/console.json').path)
+		jc = trix.jconfig(trix.npath(self.ConsoleConfig).path)
 		self.__text = jc.obj
-		self.__textk = dict(initial_indent="  * ", subsequent_indent="    ")
-		self.__textk.update(**k)
+		
+		# constructor kwargs for textwrapping
+		tk = trix.kcopy(k, self.TextK_Updates)
+		tk.setdefault("initial_indent", "  * ")
+		tk.setdefault("subsequent_indent", " "*4)
+		self.__textk = tk
+		
+		# store terminal width
 		self.__textw = terminfo.termsize()[1]
-			
+		
+		#
+		# TEXTBLOCK ARGS
+		#  - textblock arguments must be sent from the `about`, `help`, 
+		#    and console method to the `__textblock()` method as keyword 
+		#    arguments.
+		#  - The value of self.wrap may change through the course of a
+		#    console session, so that one must be set in `__textblock()`.
+		#
+		try:
+			targname = self.target.name
+		except:
+			targname = repr(self.target)
+		
+		self.__textblock_kwargs = dict(
+				wrapper_name = wrapname,
+				target_strm = targname,
+				console_cmd = self.ConsoleCmdKey,
+				wrapper_cmd = self.ConsoleWrapKey
+			)
 	
 	
 	def __del__(self):
@@ -82,6 +101,10 @@ class Console(BaseOutput):
 	@property
 	def jformat(self):
 		"""Object display format (default: JDisplay)."""
+		
+		#
+		# TO-DO: Use this (rather than `display`) to output structures!
+		#
 		try:
 			return self.__jformat
 		except:
@@ -91,8 +114,7 @@ class Console(BaseOutput):
 	@property
 	def prompt(self):
 		"""Console prompt."""
-		return "> "
-	
+		return "%s " % self.ConsolePrompt
 	
 	@property
 	def wrap(self):
@@ -102,28 +124,45 @@ class Console(BaseOutput):
 	
 	
 	#
-	# TEXT BLOCK
+	# TEXT BLOCKS (banner, about, help)
 	#
-	def _textblock(self, textkey):
+	def __textblock(self, textkey, **k):
+		
+		#trix.display (["self.text", self.__text])
+		
+		# read each line from the specified text
 		for line in self.__text[textkey]:
-			tx = textwrap.wrap(line, width=self.__textw, **self.__textk)
-			for line in tx:
-				print (line)
+			
+			# wrap the line to fit the current terminal width
+			self.__textk.setdefault('width', self.__textw)
+			tx = textwrap.wrap(line, **self.__textk)
+			
+			#trix.display (["tx", tx])
+			self.__textblock_kwargs.setdefault('wrapper_name', "None")
+			try:
+				for lx in tx:
+					print (lx.format(**self.__textblock_kwargs))
+			except Exception as ex:
+				raise type(ex)(xdata(line=line, tx=tx, lx=lx, 
+						tbkwargs=self.__textblock_kwargs
+					))
 	
-	
-	#
-	# BANNER
-	#
-	def banner(self):
-		print (self.__text['title'])
-		self._textblock('banner')
-		print ("\n")
 	
 	def about(self):
-		for line in self.__text['about']:
-			tx = textwrap.wrap(line, width=self.__textw, **self.__textk)
-			for line in tx:
-				print (line)
+		print (self.__text['title_about'])
+		self.__textblock('about')
+		print("")
+	
+	def banner(self):
+		print (self.__text['title'])
+		self.__textblock('banner')
+		print("")
+	
+	def help(self):
+		print (self.__text['title_help'])
+		self.__textblock('help')
+		print("")
+		#print("Help is not yet available.")
 	
 	
 	
@@ -154,11 +193,13 @@ class Console(BaseOutput):
 				# make sure there's some text to parse
 				if line:
 					# get and handle event
-					if line[0] == '/':
+					
+					if line[0] == self.ConsoleCmdKey:
 						# this is a command for the console object
 						e = self.create_event(line[1:])
 						self.handle_command(e)
-					elif line[0] == '!':
+					
+					elif line[0] == self.ConsoleWrapKey:
 						# this is a command for the wrapped object
 						e = self.create_event(line[1:])
 						self.handle_wrapper(e)
@@ -169,17 +210,17 @@ class Console(BaseOutput):
 			except (EOFError, KeyboardInterrupt) as ex:
 				# Ctrl-C exits this prompt with EOFError; end the session.
 				self.__active = False
-				self.output("\n#\n# Console Exit\n#\n")
+				print("\n#\n# Console Exit (%s)\n#\n" % str(type(ex)))
 			
 			except BaseException as ex:
-				self.output('') # get off the "input" line
-				self.output("#\n# %s: %s\n#" % (type(ex).__name__, str(ex)))
+				print('') # get off the "input" line <---- KEEP PRINT HERE?
+				print("#\n# %s: %s\n#" % (type(ex).__name__, str(ex)))
 				if self.debug:
 					if e and e.error:
-						self.output(self.jformat(dict(event=e.dict,err=e.error)))
-					self.output(self.jformat(xdata()))
-					#self.output(self.newl.join(trix.tracebk()))
-					self.output(self.jformat(trix.tracebk()))
+						print(self.jformat(dict(event=e.dict,err=e.error)))
+					print(self.jformat(xdata()))
+					#print(self.newl.join(trix.tracebk()))
+					print(self.jformat(trix.tracebk()))
 	
 	
 	
@@ -200,15 +241,22 @@ class Console(BaseOutput):
 	def handle_command(self, e):
 		"""
 		Handle input event `e`.
-		"""
+		"""		
+		
 		if e.argvl[0] == 'about':
 			self.about()
+
+		if e.argvl[0] == 'banner':
+			self.banner()
+		
+		if e.argvl[0] == 'help':
+			self.help()
 		
 		elif e.argvl[0] == 'debug':
 			if e.arg(1) is not None:
 				self.__debug = bool(e.arg(1))
 			else:
-				self.output(str(bool(self.__debug)))
+				print(str(bool(self.__debug)))
 		
 		# list
 		elif e.argvl[0] == 'list':
@@ -216,24 +264,32 @@ class Console(BaseOutput):
 		
 		# selected
 		elif e.argvl[0] == 'selected':
-			self.oselected()
+			o = self.oselected()
+			if o:
+				print(repr(o))
+				print()
 		
 		# select
 		elif e.argvl[0] == 'select':
 			try:
 				objectName = e.argv[1]
 				self.oselect(objectName)
+				wrapper_name = self.wrap.name if self.wrap else "None"
+				self.__textk['wrapper_name'] = wrapper_name
+
 			except IndexError:
 				print("FAIL. Argument 'object name' required.")
 		
 		elif e.argvl[0] in ['wrap', 'wrapper']:
-			wrap = type(self.__wrap.o).__name__
-			self.output("%s: %s" % (wrap, self.__wrap.name))
+			if self.__wrap:
+				wrap = type(self.__wrap).__name__
+				print("%s: %s" % (wrap, self.__wrap.name)+"\r")
+			else:
+				print()
 		
 		# exit the console session
 		elif e.argvl[0] == 'exit':
 			self.__active = False
-	
 	
 	
 	
@@ -248,12 +304,15 @@ class Console(BaseOutput):
 			# wrapper commands are case-sensitive, so use e.arg(0)
 			r = self.__wrap(e.arg(0), *e.argv[1:], **e.kwargs)
 			if r:
-				self.output(str(r))
+				print(str(r)+"\r")
 	
 	
 	
-	# -----------------------------------------------------------------
-	
+	#
+	#
+	# List, select, or [query for the selected] Runner
+	#
+	#
 	def oselected(self):
 		if self.__wrap:
 			#print (self.__wrap)
@@ -261,15 +320,20 @@ class Console(BaseOutput):
 		else:
 			print()
 	
+	
 	def oselect(self, name):
 		"""Select an object from the object list to wrap."""
 		try:
+			self.__wrap = None
 			for o in self.__olist:
 				if o.name == name:
 					self.__wrap = Wrap(o)
+			if not self.__wrap:
+				raise ValueError(xdata(error="err-select-fail", 
+						reason="no-such-name", name=name
+					))
 		except Exception as ex:
 			print (type(ex), ex.args)
-			
 	
 	
 	@property
@@ -282,3 +346,35 @@ class Console(BaseOutput):
 			pass
 		return trix.propx(r)
 	
+	
+	#
+	# CLASSMETHODS USED BY RUNNER TO ADD/REMOVE SELF
+	#
+	@classmethod
+	def oappend(cls, o):
+		try:
+			cls.__olist.append(o)
+		except:
+			cls.__olist = []
+			cls.__olist.append(o)
+	
+	@classmethod
+	def oremove(cls, o):
+		try:
+			cls.__olist.remove(o)
+		except:
+			cls.__olist = []
+	
+	
+	# AT EXIT
+	@classmethod
+	def _at_exit(cls):
+		for obj in cls.__olist:
+			obj.shutdown()
+
+#
+# Make all runners shutdown when the program terminates.
+#
+atexit.register(Console._at_exit)
+
+
