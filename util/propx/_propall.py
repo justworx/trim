9bb3be13ab24fec814306862d20f517e954a8cc3
justworx <justworx@protonmail.com>
2020-10-10 19:37:49 -0500

    make 'help' access easier

---
+++ util/propx/_propall.py
@@ -0,0 +1,15 @@
+#
+# PROP-ALL
+#  - This module is a convenience allowing easy import of all
+#    propx-based classes. It is used in the documentation of 
+#    the `trix.propx` classmethod to make studying the propx
+#    class documentation easier.
+#
+
+from . import *
+
+from .propdict import *
+from .propiter import *
+from .proplist import *
+from .propseq  import *
+from .propstr import *
