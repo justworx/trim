2a8376f2739ee453194e8aa737b137e5d0af0c06
justworx <justworx@protonmail.com>
2020-10-11 01:51:10 -0500

    comments/doc/examples

--- util/propx/propdict.py
+++ util/propx/propdict.py
@@ -7,7 +7,6 @@
 from .proplist import *
 
 
-#class propdict(propbase):
 class propdict(propiter):
 	"""
 	Pass a dict or dict-like object.
@@ -19,16 +18,42 @@ class propdict(propiter):
 	def __setitem__(self, key, v):
 		self.o[key] = v
 	
+	
 	@property
 	def keys(self):
+		"""
+		Returns a proplist with dict keys.
+		
+		EXAMPLE
+		>>>
+		>>> pd = propdict({'a':1, 'p':'q'})
+		>>> pd.keys.o
+		['a', 'p']
+		>>> 
+		
+		"""
 		return proplist(list(self.o.keys()))
 	
 	@property
 	def values(self):
+		"""
+		Returns a proplist with dict values.
+		"""
 		return proplist(list(self.o.values()))
 	
 	@property
 	def pairs(self):
+		"""
+		Returns a proplist with key/value pairs from dict.
+		
+		EXAMPLE
+		>>>
+		>>> pd = propdict({'a':1, 'p':'q'})
+		>>> pd.pairs()
+		[('a', 1), ('p', 'q')]
+		>>> 
+		
+		"""
 		r = []
 		for k in self.o.keys():
 			r.append((k, self.o[k]))
@@ -36,11 +61,20 @@ class propdict(propiter):
 	
 	@property
 	def rpairs(self):
+		"""
+		Returns a proplist with key/value pairs reversed, making them
+		value/key pairs.
+		
+		EXAMPLE
+		>>>
+		>>> pd = propdict({'a':1, 'p':'q'})
+		>>> pd.pairs()
+		[(1, 'a'), ('q', 'p')]
+		>>> 
+		"""
 		r = []
 		for k in self.o.keys():
 			r.append((self.o[k], k))
 		return proplist(r)
-		
-		#return proplist(list(enumerate(self.o)))
 	
 
