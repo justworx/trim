aa44ca80c314309ca588e72f07811f13d617e55b
justworx <justworx@protonmail.com>
2020-11-06 00:45:30 -0600

    load plugins from config; add comments, formatting; add handle_input

--- util/console.py
+++ util/console.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 justworx
+# Copyright 2018-2020 justworx
 # This file is part of the trix project, distributed under
 # the terms of the GNU Affero General Public License.
 #
@@ -18,10 +18,19 @@ from .wrap import *
 
 class Console(BaseOutput):
 	"""
-	Base class for an interactive command-line user interface.
+	An interactive command-line user interface.
+	
+	Console is under construction, but is useful for debugging.
+	It provides a measure of control over objects based on the 
+	`util.runner.Runner` class.
+	
+	EXAMPLE:
+	>>> from trix.util.console import *
+	>>> Console().console()
+	
 	"""
 	
-	Debug = 1 #0
+	Debug = False
 	
 	# need to make these defaults for `config`
 	ConsoleCmdKey  = '/'
@@ -29,6 +38,9 @@ class Console(BaseOutput):
 	ConsolePrompt  = "trix.Console:"
 	ConsoleConfig  = "app/config/assets/console.%s" % DEF_LANG
 	
+	#
+	# 
+	#
 	TextK_Updates  = ["width", "expand_tabs", "tabsize", 
 		"replace_whitespace", "drop_whitespace", "initial_indent", 
 		"subsequent_indent", "fix_sentence_endings", "break_long_words",
@@ -47,12 +59,15 @@ class Console(BaseOutput):
 	#
 	#
 	def __init__(self, config=None, **k):
+		"""
+		Create a `Console` object.
 		
-		#print (type(config))
+		Though config and keyword arguments may be provided, it probably
+		is best, for now, to open the console using defaults.
 		
-		# OOPS... why?              <-- probably don't need this
-		#type(self).__olist = []
 		
+		
+		"""
 		config = config or {}
 		config.update(k)
 		
@@ -61,6 +76,18 @@ class Console(BaseOutput):
 		
 		# set debugging
 		self.__debug = config.get('debug', self.Debug)
+		
+		# plugins
+		#self.__plugins = {}
+		#plugin_config = trix.config(DEF_CACHE
+		
+		self.__plugins = {}
+		self.__plugins['calc'] = trix.ncreate(
+				"app.plugin.calc.Calc", "calc", self, {}
+			)
+		self.__plugins['dir'] = trix.ncreate(
+				"app.plugin.dir.Dir", "dir", self, {}
+			)
 		
 		#
 		#
@@ -78,7 +105,7 @@ class Console(BaseOutput):
 		self.__wrap = Wrap(wrap) if wrap else None
 		
 		# set wrapper name
-		wrapname = self.__wrap.name if self.__wrap else "None"
+		wrapname = self.__wrap.name if self.__wrap else None # was "None"
 		
 		# formatting for textwrap
 		jc = trix.jconfig(trix.npath(self.ConsoleConfig).path)
@@ -92,6 +119,14 @@ class Console(BaseOutput):
 		
 		# store terminal width
 		self.__textw = terminfo.termsize()[1]
+		
+		
+		# prompt
+		self.__prompt = jc.obj.get('prompt') or self.ConsolePrompt
+		#ConsoleCmdKey  = '/'
+		#ConsoleWrapKey = '!'
+		#ConsolePrompt  = "trix.Console:"
+		
 		
 		#
 		# TEXTBLOCK ARGS
@@ -114,20 +149,34 @@ class Console(BaseOutput):
 			)
 	
 	
+	#
+	#
+	# __DEL__
+	#
+	#
 	def __del__(self):
 		self.__wrap = None
 	
 	
-	
+	#
+	#
+	# DEBUG
+	#
+	#
 	@property
 	def debug(self):
 		"""Debug setting."""
 		return self.__debug
 	
+	
+	#
+	#
+	# JFORMAT
+	#
+	#
 	@property
 	def jformat(self):
 		"""Object display format (default: JDisplay)."""
-		
 		#
 		# TO-DO: Use this (rather than `display`) to output structures!
 		#
@@ -137,20 +186,44 @@ class Console(BaseOutput):
 			self.__jformat = trix.formatter(f='JDisplay')
 			return self.__jformat
 	
+	
+	#
+	#
+	# PROMPT
+	#
+	#
 	@property
 	def prompt(self):
 		"""Console prompt."""
-		return "%s " % self.ConsolePrompt
-	
-	@property
-	def wrap(self):
-		"""Current wrapped object."""
-		return self.__wrap
-	
+		return "%s " % self.__prompt
 	
 	
 	#
-	# TEXT BLOCKS (banner, about, help)
+	#
+	# WRAP
+	#
+	#
+	@property
+	def wrap(self):
+		"""Currently selected "wrapped" object."""
+		return self.__wrap
+	
+	
+	#
+	#
+	# PLUGINS
+	#
+	#
+	@property
+	def plugins(self):
+		"""Return plugins dict."""
+		return self.__plugins
+	
+	
+	#
+	#
+	# __TEXTBLOCK - (banner, about, help)
+	#
 	#
 	def __textblock(self, textkey, **k):
 		
@@ -174,16 +247,33 @@ class Console(BaseOutput):
 					))
 	
 	
+	#
+	#
+	# ABOUT
+	#
+	#
 	def about(self):
 		print (self.__text['title_about'])
 		self.__textblock('about')
 		print("")
 	
+	
+	#
+	#
+	# BANNER
+	#
+	#
 	def banner(self):
 		print (self.__text['title'])
 		self.__textblock('banner')
 		print("")
 	
+	
+	#
+	#
+	# HELP
+	#
+	#
 	def help(self):
 		print (self.__text['title_help'])
 		self.__textblock('help')
@@ -191,14 +281,14 @@ class Console(BaseOutput):
 		#print("Help is not yet available.")
 	
 	
-	
 	#
-	# CREATE EVENT
+	#
+	# CREATE_EVENT
+	#
 	#
 	def create_event(self, commandLineText):
 		"""Returns a TextEvent."""
 		return LineEvent(commandLineText)
-	
 	
 	
 	#
@@ -207,7 +297,15 @@ class Console(BaseOutput):
 	#
 	#
 	def console(self):
-		"""Call this method to start a console session."""
+		"""
+		Call this method to start a console session.
+		
+		What happens?
+		 * A banner is displayed.
+		 * A prompt is displayed.
+		 * Prompt commands are executed.
+		 
+		"""
 		self.banner()
 		self.__active = True
 		while self.__active:
@@ -216,22 +314,41 @@ class Console(BaseOutput):
 				# get input, create Event
 				line = xinput(self.prompt).strip()
 				
-				# make sure there's some text to parse
+				# Make sure there's some text to parse.
 				if line:
-					# get and handle event
+					#
+					# Get and handle whatever text was entered.
+					#
 					
+					#
+					# SEND TO HANDLE-COMMAND: /
+					#  - If the first character on the (stripped) line is a
+					#    slash '/', this is a command for the console.
+					#
 					if line[0] == self.ConsoleCmdKey:
-						# this is a command for the console object
 						e = self.create_event(line[1:])
 						self.handle_command(e)
 					
+					#
+					# SEND TO HANDLE-WRAPPER: !
+					#  - If the first character on the (stripped) line is a
+					#    bang '!', this is a command for the currently "wrapped"
+					#    object (which can be selected by /wrap <object name>).
+					#
 					elif line[0] == self.ConsoleWrapKey:
-						# this is a command for the wrapped object
 						e = self.create_event(line[1:])
 						self.handle_wrapper(e)
+					
+					#
+					# SEND TO HANDLE-INPUT:
+					#  - When the first character on the (stripped) line is 
+					#    neither bang '!' nor slash '/', the command is handled
+					#    differently.
+					#
 					else:
 						e = self.create_event(line)
 						self.handle_input(e)
+			
 			
 			except (EOFError, KeyboardInterrupt) as ex:
 				# Ctrl-C exits this prompt with EOFError; end the session.
@@ -257,25 +374,105 @@ class Console(BaseOutput):
 	#
 	#
 	
+	
+	#
+	#
+	# HANDLE_INPUT
+	#
+	#
 	def handle_input(self, e):
-		"""Send `e.text` to the target stream."""
-		self.output(e.text)
+		"""
+		Send `e.text` to the target stream.
+		"""
+		if e.text:
+			try:
+				self.plugins['calc'].handle(e)
+			except BaseException as ex:
+				self.output(str(ex)) # comment this ( debugging )sin
+				pass
+			
+			x = self.plugins['dir']
+			
+			#
+			# The Dir plugin publishes its command list in a class variable.
+			# This is a good idea to remember for plugins that have only
+			# commands (as opposed to calc, which watches every line for
+			# a chance to do its thing.
+			#
+			if e.text in x.COMMANDS:
+				x.handle(e)
+			
+			if e.reply:
+				self.output(e.reply)
+			
+		if self.debug:
+			e.display()
 	
 	
-	
-	
+	#
+	#
+	# HANDLE_COMMAND
+	#
+	#
 	def handle_command(self, e):
 		"""
 		Handle input event `e`.
+		
+		The `handle_command` method is called when `ConsoleCmdKey` is the 
+		first character in a given command line. (Default: "/".)
+		
+		FOR EXAMPLE:
+		/help causes the help banner to be displayed
+		
+		EXAMPLE
+		trix.Console: /help
+		Console commands/descriptions
+		  * about    : Print the 'about' message
+		  * banner   : Print the console startup banner
+		  * help     : Print (this) help message
+		  * list     : List Runners by name
+		  * select   : Select a Runner by name
+		  * selected : Display name of currently selected Runner
+		  * exit     : Exit the console.
+		
+		SEE ALSO:
+		>>> from trix.util.runner import *
+		>>> help(Runner)
+		
+		NOTE ALSO:
+		Objects in this process that are based on `util.runner.Runner` 
+		automatically show up in the /list results. Use /select to select
+		a object by name. The /selected command displays the currently 
+		selected object.
+		
+		Use !<command> feature to query `Runner.display` and other 
+		methods.
+		
 		"""		
 		
-		if e.argvl[0] == 'about':
+		#
+		# UNDOCUMENTED COMMANDS
+		#
+		if e.argvl[0] == 'plugins':
+			trix.display(self.plugins)
+		
+		elif e.argvl[0] == 'prompt':
+			self.__prompt = e.argv[1]
+		
+		elif e.argvl[0] == 'clear':
+			trix.tx.cls
+		
+		
+		#
+		# DOCUMENTED COMMANDS
+		#
+		elif e.argvl[0] == 'about':
 			self.about()
 
-		if e.argvl[0] == 'banner':
+		elif e.argvl[0] == 'banner':
 			self.banner()
 		
-		if e.argvl[0] == 'help':
+		elif e.argvl[0] == 'help':
 			self.help()
 		
 		elif e.argvl[0] == 'debug':
@@ -304,7 +501,7 @@ class Console(BaseOutput):
 				self.__textk['wrapper_name'] = wrapper_name
 
 			except IndexError:
-				print("FAIL. Argument 'object name' required.")
+				print("FAIL. Argument <object-name> required.")
 		
 		elif e.argvl[0] in ['wrap', 'wrapper']:
 			if self.__wrap:
@@ -318,7 +515,11 @@ class Console(BaseOutput):
 			self.__active = False
 	
 	
-	
+	#
+	#
+	# HANDLE_WRAPPER
+	#
+	#
 	def handle_wrapper(self, e):
 		"""
 		Handle commands prefixed with the exclamation point '!' 
@@ -333,10 +534,10 @@ class Console(BaseOutput):
 				print(str(r)+"\r")
 	
 	
-	
 	#
 	#
-	# List, select, or [query for the selected] Runner
+	# OSELECTED
+	#  - List, select, or [query for the selected] Runner
 	#
 	#
 	def oselected(self):
@@ -347,6 +548,11 @@ class Console(BaseOutput):
 			print()
 	
 	
+	#
+	#
+	# OSELECT
+	#
+	#
 	def oselect(self, name):
 		"""Select an object from the object list to wrap."""
 		try:
@@ -359,10 +565,16 @@ class Console(BaseOutput):
 						reason="no-such-name", name=name
 					))
 		except Exception as ex:
-			raise
 			print (type(ex), ex.args)
+			if self.debug:
+				raise
 	
 	
+	#
+	#
+	# OLIST
+	#
+	#
 	@property
 	def olist(self):
 		try:
@@ -371,14 +583,18 @@ class Console(BaseOutput):
 				r.append(o.name)
 			
 			return trix.propx(r)
-		except:
-			print (type(ex), ex.args)
-			raise
 		
+		except BaseException as ex:
+			print (type(ex), ex.args)
+			if self.debug:
+				raise
 	
 	
 	#
-	# CLASSMETHODS USED BY RUNNER TO ADD/REMOVE SELF
+	#
+	# OAPPEND
+	#  - Classmethods used by runner to add/remove self
+	#
 	#
 	@classmethod
 	def oappend(cls, o):
@@ -389,6 +605,12 @@ class Console(BaseOutput):
 			cls.__olist = []
 			cls.__olist.append(o)
 	
+	
+	#
+	#
+	# OREMOVE
+	#
+	#
 	@classmethod
 	def oremove(cls, o):
 		try:
@@ -398,7 +620,11 @@ class Console(BaseOutput):
 			cls.__olist = []
 	
 	
+	#
+	#
 	# AT EXIT
+	#
+	#
 	@classmethod
 	def _at_exit(cls):
 		for obj in cls.__olist:
