36aefb43e8184b277aaeead483822c0f68ad7f04
justworx <justworx@protonmail.com>
2020-11-14 17:31:15 -0600

    Playing with experimental tbase module. not very sure about it. it doesn't seem as useful as I'd imagined.

--- util/tbase.py
+++ util/tbase.py
@@ -11,51 +11,71 @@ from .tbase import *
 
 class TBase(EncodingHelper):
 	"""
+	EXPERIMENTAL
+	
 	A useful base class that exposes a common interface for basic 
 	needs, as well as methods that may be useful in the interpreter 
 	and for debugging/development.
+	
+	...Or, at least, that's what I'm hoping for.
+	
 	"""
 	
 	def __init__(self, *a, **k):
+		
+		k.setdefault('encoding', DEF_ENCODE)
+		k.setdefault('errors', DEF_ERRORS)
+		
 		self.__a = a
 		self.__k = k
+		
+		EncodingHelper.__init__(self, *a, **k)
+	
 	
 	@property
 	def a(self):
-		"""For reference: Return any given args."""
-		return self.__a
+		"""
+		Returns arguments given to the `TBase` constructor, wrapped in a 
+		proplist.
+		
+		If you call this property as a method it returns the original
+		arguments as a list.
+		
+		"""
+		return trix.propx(list(self.__a))
 	
-	@property
-	def ax(self):
-		"""
-		For reference: Return any given args wrapped in propx().
-		"""
-		return trix.propx(self.__a)
 	
 	@property
 	def k(self):
 		"""
+		Returns keyword arguments given to the TBase constructor, wrapped
+		in a proplist.
+		
+		If you call this property as a method, it returns the original
+		arguments as a list.
+		
 		For reference: Return any given keyword args.
-		"""
-		return self.__k
-	
-	@property
-	def kx(self):
-		"""
-		For reference: Return any given keyword args wrapped in propx().
 		"""
 		return trix.propx(self.__k)
 	
+	
 	@property
-	def ekx(self):
+	def ee(self):
 		"""
 		For reference: Return the contents of EncodingHelper data
 		wrapped in a propx.
 		"""
-		return trix.propx(self.__k)
+		return trix.propx(self.ek)
+	
 	
 	@property
 	def term(self, **k):
-		"""Return a terminal."""
-		return trix.ncreate('util.terminal.Terminal', **k)		
+		"""
+		Return the terminal object.
+		"""
+		try:
+			return self.__term
+		except:
+			self.__term = trix.ncreate('util.terminal.Terminal', **k)
+			return self.__term	
 	
