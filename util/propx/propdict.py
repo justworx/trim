54869160c89021743fcd8f913e3fa66a80d640b1
justworx <justworx@protonmail.com>
2020-11-16 16:03:03 -0600

    examples, doc

--- util/propx/propdict.py
+++ util/propx/propdict.py
@@ -10,6 +10,18 @@ from .proplist import *
 class propdict(propiter):
 	"""
 	Pass a dict or dict-like object.
+	
+	EXAMPLE
+	>>> import trix
+	>>> d = trix.propx({"a":1, "b":9, "c":4})
+	>>> d.display()
+	{
+	  "a": 1,
+	  "b": 9,
+	  "c": 4
+	}
+	>>>	
+	
 	"""
 	
 	#
@@ -18,6 +30,22 @@ class propdict(propiter):
 	#
 	#
 	def __getitem__(self, key):
+		"""
+		Return an item by `key`.
+		
+		NOTE:
+		All propx-based classes' `__getitem__` methods return a propx 
+		object.
+		
+		EXAMPLE
+		>>> import trix
+		>>> d = trix.propx({"a":1, "b":9, "c":4})
+		>>> d['b']
+		<trix/propbase int>
+		>>> d['b'].o
+		9
+		>>>	
+		"""
 		return propx(self.o[key])
 	
 	
@@ -27,6 +55,22 @@ class propdict(propiter):
 	#
 	#
 	def __setitem__(self, key, v):
+		"""
+		Set an item by `key`.
+		
+		EXAMPLE
+		>>> import trix
+		>>> d = trix.propx({"a":1, "b":9, "c":4})
+		>>> d['b'] = "nine"
+		>>> d.display()
+		{
+		  "a": 1,
+		  "b": "nine",
+		  "c": 4
+		}
+		>>>
+		
+		"""
 		self.o[key] = v
 	
 	
@@ -41,7 +85,7 @@ class propdict(propiter):
 		Returns a proplist with dict keys.
 		
 		EXAMPLE
-		>>>
+		>>> import trix
 		>>> pd = propdict({'a':1, 'p':'q'})
 		>>> pd.keys.o
 		['a', 'p']
@@ -60,6 +104,13 @@ class propdict(propiter):
 	def values(self):
 		"""
 		Returns a proplist with dict values.
+		
+		EXAMPLE
+		>>> import trix
+		>>> pd = trix.propx({'a':1, 'p':'q'})
+		>>> pd.values()
+		[1, 'q']
+		>>> 
 		"""
 		return propx(list(self.o.values()))
 	
@@ -75,10 +126,14 @@ class propdict(propiter):
 		Returns a proplist with key/value pairs from dict.
 		
 		EXAMPLE
-		>>>
-		>>> pd = propdict({'a':1, 'p':'q'})
+		>>> import trix
+		>>> pd = trix.propx({'a':1, 'p':'q'})
 		>>> pd.pairs()
 		[('a', 1), ('p', 'q')]
+		>>> 
+		>>> d = trix.propx({"a":1, "b":9, "c":4})
+		>>> d.pairs()
+		[('a', 1), ('b', 9), ('c', 4)]
 		>>> 
 		
 		"""
@@ -100,10 +155,14 @@ class propdict(propiter):
 		value/key pairs.
 		
 		EXAMPLE 1
-		>>> from trix.util.propx.propdict import *
-		>>> pd = propdict({'a':1, 'p':'q'})
+		>>> import trix
+		>>> pd = trix.propx({'a':1, 'p':'q'})
 		>>> pd.pairs()
 		[(1, 'a'), ('q', 'p')]
+		>>> 
+		>>> d = trix.propx({"a":1, "b":9, "c":4})
+		>>> d.rpairs()
+		[(1, 'a'), (9, 'b'), (4, 'c')]
 		>>> 
 		
 		"""
